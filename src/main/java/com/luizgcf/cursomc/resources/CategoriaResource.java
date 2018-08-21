1c7b686d70246724a6e5bcc948a46f3b441c842f
Luiz Gonzaga <lgcf97@gmail.com>
2018-08-21 12:25:49 -0300

    Testing first domain class | Categories

--- src/main/java/com/luizgcf/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luizgcf/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.luizgcf.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luizgcf.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar(){
-		return "Ok";
+	public List<Categoria> listar(){
+		Categoria c1 = new Categoria(1,"Informática");
+		Categoria c2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> l = new ArrayList<>();
+		l.add(c1);
+		l.add(c2);
+		
+		
+		return l;
 	}
 }
