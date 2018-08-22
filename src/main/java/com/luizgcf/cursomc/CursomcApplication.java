bbd7e1e249910b79b86ae7e505a3283a9df72809
Luiz Gonzaga <lgcf97@gmail.com>
2018-08-22 09:31:38 -0300

    Product and Many to Many association

--- src/main/java/com/luizgcf/cursomc/CursomcApplication.java
+++ src/main/java/com/luizgcf/cursomc/CursomcApplication.java
@@ -8,10 +8,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.luizgcf.cursomc.domain.Categoria;
+import com.luizgcf.cursomc.domain.Produto;
 import com.luizgcf.cursomc.repositories.CategoriaRepository;
+import com.luizgcf.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -26,6 +31,19 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2)); 
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 }
