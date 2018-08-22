4514be71691467f13a0481fd6f7441ee8d6994f8
Luiz Gonzaga <lgcf97@gmail.com>
2018-08-22 10:05:01 -0300

    Endpoint /categorias/{id} adjustments

--- src/main/java/com/luizgcf/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/luizgcf/cursomc/repositories/CategoriaRepository.java
@@ -1,7 +1,5 @@
 package com.luizgcf.cursomc.repositories;
 
-import java.io.Serializable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/luizgcf/cursomc/services/CategoriaService.java
+++ src/main/java/com/luizgcf/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.luizgcf.cursomc.domain.Categoria;
 import com.luizgcf.cursomc.repositories.CategoriaRepository;
+import com.luizgcf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: "+id+", Tipo: "+Categoria.class.getName()));
 	}
 }
