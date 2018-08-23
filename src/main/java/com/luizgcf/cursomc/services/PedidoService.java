a192f4116707ef542751f3d8c639662e56d13707
Luiz Gonzaga <lgcf97@gmail.com>
2018-08-23 14:50:56 -0300

    Endpoint /pedidos/{id} available

---
+++ src/main/java/com/luizgcf/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+	package com.luizgcf.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizgcf.cursomc.domain.Pedido;
+import com.luizgcf.cursomc.repositories.PedidoRepository;
+import com.luizgcf.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: "+id+", Tipo: "+Pedido.class.getName()));
+	}
+}
