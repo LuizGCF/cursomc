1437adee915b16cc36f6823581910e3743201a8b
Luiz Gonzaga <lgcf97@gmail.com>
2018-08-23 08:50:11 -0300

    Endpoint /clientes/{id} available

---
+++ src/main/java/com/luizgcf/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.luizgcf.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizgcf.cursomc.domain.Cliente;
+import com.luizgcf.cursomc.repositories.ClienteRepository;
+import com.luizgcf.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: "+id+", Tipo: "+Cliente.class.getName()));
+	}
+}
